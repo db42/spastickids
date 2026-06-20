76554a37451caf7b789016986af377f36a0f93f0
Dushyant Bansal <dushyant37@gmail.com>
2026-06-20 17:27:44 +0530

    Update site title, homepage, footer, and about page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SpasticKids';
+export const SITE_DESCRIPTION = 'Learnings from raising a child with spastic diplegia.';
